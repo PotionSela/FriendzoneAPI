4970c75a90a489bba5e7e5e743b99fa667c7ead3
Gisela <giselamata27@gmail.com>
2024-02-16 12:51:36 -0500

    Added logic to the reaction model

---
+++ models/reaction.js
@@ -0,0 +1,27 @@
+// Requiring Mongoose
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+    });
+
+    module.exports = reactionSchema;
