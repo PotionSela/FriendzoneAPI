e57d7ccbf1a7ff01c4d18164c8b0e11bdcf8f81a
Gisela <giselamata27@gmail.com>
2024-02-19 12:05:33 -0500

    Get route for all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,14 @@
 // Importing User, and Thoughts models
 const { User, Thought } = require('../models');
 
+module.exports = {
+    // Get all users
+    async getUser(req, res) {
+        try {
+            const user = await User.find().populate('thought');
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+}
