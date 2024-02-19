0e086c7c50a83257e708f354e254e0e22fbd8d93
Gisela <giselamata27@gmail.com>
2024-02-19 13:03:35 -0500

    Fixed up small spelling errors and added more comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,20 +3,21 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     // Get all users by id
-    async getUser(req, res) {
+    async getUsers(req, res) {
         try {
             // Get Id, and populate thought and friend data
-            const user = await User.find().populate('thought').populate('friends');
-            res.json(user);
+            const users = await User.find().populate('thought').populate('friends');
+            res.json(users);
         } catch (err) {
             res.status(500).json(err);
         }
     },
+
     // Get a single user
     async getSingleUser(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId })
-            .populate('thought');
+            .populate('thoughts');
 
             if (!user) {
                 return res.status(400).json({ message: 'No user with that ID!'});
@@ -26,16 +27,18 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
     // Create a user
     async createUser(req, res) {
         try {
             const user = await User.create(req.body);
-            res.json(user);
+            res.status(200).json(user);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
         }
     },
+
     // Update user by user Id
     async updateUser(req, res) {
         try {
@@ -45,11 +48,13 @@ module.exports = {
             res.status(400).json(err);
         }
     },
+
     // Delete route to remove user by its _Id
     async deleteUser(req, res) {
         try {
             const user = await User.findByIdAndDelete(req.params.userId);
             // Remove a user's associated thoughts when deleted
+            await Thought.deleteMany({ _id: { $in: user.thoughts} });
             res.json(user);
         } catch (err) {
             res.status(400).json(err);
@@ -59,8 +64,8 @@ module.exports = {
     // Post to add a new friend to a user's friend list
     async addFriend(req, res) {
         try {
-            const user = await User.findByIdAndDelete(req.params.userId, 
-                { _id: req.params.friendId },
+            const user = await User.findByIdAndDelete(
+                req.params.userId, 
                 { $addToSet: { friends: req.params.friendId } }, 
                 { new: true } );
 
@@ -77,8 +82,8 @@ module.exports = {
     async removeFriend(req, res) {
         try {
             const user = await User.findOneAndUpdate(
-                { _id: req.params.userId },
-                { $pull: { friend: { friendId: req.params.friendId} }},
+                req.params.userId,
+                { $pull: { friends: req.params.friendId} },
                 { new: true }
             );
             
