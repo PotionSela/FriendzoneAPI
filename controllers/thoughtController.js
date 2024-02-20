7c62c352b1f743c172edf1d2552f77eda603fe2a
Gisela <giselamata27@gmail.com>
2024-02-20 13:29:25 -0500

    Added in comments and corrected some 200 successful statuses

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,6 +7,8 @@ module.exports = {
         try {
             const thoughts = await Thought.find();
             return res.status(200).json(thoughts);
+
+            // If theres a server error, log it
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
@@ -19,10 +21,12 @@ module.exports = {
             const thought = await Thought.findOne({ _id: req.params.thoughtId });
             
             if (!thought) {
-                return res.status(404).json({ message: 'No thought with that ID!'});
+                return res.status(400).json({ message: 'No thought with that ID!'});
             }
             
             return res.status(200).json(thought);
+
+            // Server error, catch it and log it
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
@@ -40,6 +44,8 @@ module.exports = {
                 { runValidators: true, new: true });
 
             return res.status(200).json({thought, user});
+
+            // For server error, catch it and log it
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
@@ -60,6 +66,8 @@ module.exports = {
             }
 
             return res.status(200).json(thought);
+
+            // If there's a server error, catch and log it
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
@@ -74,7 +82,10 @@ module.exports = {
             if (!thought) {
                 return res.status(400).json({ message: 'No thought with that Id!'});
             }
+            // Successful delete route message
             return res.status(200).json({ message: "Thought and associated reactions successfully deleted! "});
+
+            // If there's a server error, catch and log it
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
@@ -94,6 +105,8 @@ module.exports = {
                 return res.status(400).json({ message: "No thought with that Id!" });
             }
             return res.status(200).json(reaction);
+
+            // Server error, catch it and log it
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
@@ -110,11 +123,14 @@ module.exports = {
             );
             
             if(!reaction) { 
+                // If no reaction matches, send this error message
                 return reaction.status(400).json
                 ({ message: "Check thought and reaction Id please!"});
             }
 
             return res.status(200).json(reaction);
+
+            // If there's a server error, catch and log it
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
